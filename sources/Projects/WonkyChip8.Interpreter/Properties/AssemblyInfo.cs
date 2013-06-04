151bea6950b92b738e9f4de317b682aa865a8451
valmaev <almaev.vladimir@gmail.com>
2013-06-05 00:25:24 +0400

    Removed redundant comments from AssemblyInfo.

--- sources/Projects/WonkyChip8.Interpreter/Properties/AssemblyInfo.cs
+++ sources/Projects/WonkyChip8.Interpreter/Properties/AssemblyInfo.cs
@@ -3,9 +3,6 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("WonkyChip8.Interpreter")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -14,26 +11,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © VAlmaev 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-[assembly: CLSCompliant(true)]
-[assembly: InternalsVisibleTo("WonkyChip8.Interpreter.UnitTests")]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d9f10043-ae77-4094-b1d7-641a1687e102")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
+[assembly: Guid("d9f10043-ae77-4094-b1d7-641a1687e102")]
+[assembly: CLSCompliant(true)]
+[assembly: InternalsVisibleTo("WonkyChip8.Interpreter.UnitTests")]
--- sources/Tests/WonkyChip8.Interpreter.UnitTests/Properties/AssemblyInfo.cs
+++ sources/Tests/WonkyChip8.Interpreter.UnitTests/Properties/AssemblyInfo.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("WonkyChip8.Interpreter.UnitTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -13,21 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © VAlmaev 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("aa2814be-03b9-4ff1-ac36-3361c5db4fa7")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
+[assembly: Guid("aa2814be-03b9-4ff1-ac36-3361c5db4fa7")]
